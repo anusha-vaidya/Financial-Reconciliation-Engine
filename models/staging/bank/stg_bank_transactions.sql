364582e632384d1efd19d3af8848ca643b802bb6
Anusha Vaidya <vaidyaanu96@gmail.com>
2026-06-08 14:23:20 -0400

    Update stg_bank_transactions.sql

--- models/staging/bank/stg_bank_transactions.sql
+++ models/staging/bank/stg_bank_transactions.sql
@@ -1,1 +1,32 @@
+with source as (
+
+    select
+        payout_id,
+        cast(payout_date as date) as payout_date,
+        cast(gross_amount as numeric) as gross_amount,
+        cast(fee_amount as numeric) as fee_amount,
+        cast(net_amount as numeric) as net_amount,
+        currency,
+        account_id,
+        related_transaction_id
+    from {{ ref('payouts') }}
+
+),
+
+cleaned as (
+
+    select
+        payout_id,
+        payout_date,
+        gross_amount,
+        fee_amount,
+        net_amount,
+        currency,
+        account_id,
+        related_transaction_id
+    from source
+
+)
+
+select * from cleaned;
 
