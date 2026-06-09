3e3ef231de007c58119bacd9be94b722b1ee1ce9
Anusha Vaidya <vaidyaanu96@gmail.com>
2026-06-09 14:02:40 -0400

    Update stg_ledger_entries.sql

--- models/staging/ledger/stg_ledger_entries.sql
+++ models/staging/ledger/stg_ledger_entries.sql
@@ -1,1 +1,39 @@
+{{ config(materialized='view') }}
+
+with source as (
+    select
+        ledger_entry_id,
+        entry_date,
+        amount,
+        currency,
+        description,
+        account_id,
+        entry_type,
+        reference_id
+    from {{ ref('ledger_entries') }}
+),
+
+cleaned as (
+    select
+        ledger_entry_id,
+        entry_date,
+        amount,
+        currency,
+        description,
+        account_id,
+        lower(entry_type) as entry_type,
+        reference_id
+    from source
+)
+
+select
+    ledger_entry_id,
+    entry_date,
+    amount,
+    currency,
+    description,
+    account_id,
+    entry_type,
+    reference_id
+from cleaned
 
