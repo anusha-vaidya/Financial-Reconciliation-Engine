73892109c9900fd2497a6301bdf3f0b3ddf6dafa
Anusha Vaidya <vaidyaanu96@gmail.com>
2026-06-09 14:03:35 -0400

    Update stg_payouts.sql

--- models/staging/payouts/stg_payouts.sql
+++ models/staging/payouts/stg_payouts.sql
@@ -1,1 +1,39 @@
+{{ config(materialized='view') }}
+
+with source as (
+    select
+        payout_id,
+        payout_date,
+        gross_amount,
+        fee_amount,
+        net_amount,
+        currency,
+        account_id,
+        transaction_id
+    from {{ ref('payouts') }}
+),
+
+cleaned as (
+    select
+        payout_id,
+        payout_date,
+        gross_amount,
+        fee_amount,
+        net_amount,
+        currency,
+        account_id,
+        transaction_id
+    from source
+)
+
+select
+    payout_id,
+    payout_date,
+    gross_amount,
+    fee_amount,
+    net_amount,
+    currency,
+    account_id,
+    transaction_id
+from cleaned
 
